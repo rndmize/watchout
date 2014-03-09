33a78f45a44c3ce25c5569e80de1cc522ad66c32
Hack Reactor Students <rndmize@gmail.com>
2014-03-08 17:59:32 -0800

    More work

--- watchout.js
+++ watchout.js
@@ -2,7 +2,7 @@ var score = 0;
 var highScore = 0;
 var collisions = 0;
 
-var dots = d3.range(50);
+var dots = d3.range(5);
 for (var i = 0; i < dots.length; i++) {
   dots[i] = { x: 0, y: 0 }
 }
@@ -49,24 +49,47 @@ var setHighScore = function() {
 var setCollisions = function() {
   if(!collided) {
     collided = true;
-    d3.select('.svg-wrap').transition().duration(100).style('background-color', 'red');
     d3.select('.collisions').select('span').text(++collisions);
-    setTimeout(function(){
-      d3.select('.svg-wrap').transition().duration(100).style('background-color', '#f9f9f9');
-      collided = false;
+    ring();
+      setTimeout(function(){
+        collided = false;
     }, 100);
   }
-}
+};
+
+var ring = function() {
+  player.attr({'stroke': 'red',
+      'stroke-width': 0})
+    .transition()
+    .duration(250)
+    .attr({'stroke': '#f9f9f9',
+      'stroke-width': 80});
+};
+
+var hkDraw = function(x, y) {
+  var a = [x - 10, x, x + 10, x + 10, x, x - 10];
+  var b = [y + 5.75, y + 11.5, y + 5.75, y - 5.75, y - 11.5, y - 5.75];
+  var str = '' + a[0] + ',' + b[0];
+  for (var i = 1; i < 6; i++) {
+    str += ' ' + a[i] + ',' + b[i];
+  }
+  d3.select('.hk').attr({cx: x, cy: y});
+  return str;
+};
 
 var setScore = function() {
   d3.select('.current').select('span').text(score);
 };
 
 var checkCollisions = function(endData) {
-  // debugger;
   var enemy = d3.select(this);
-  var enemyEndX = endData.x;
-  var enemyEndY = endData.y;
+  // console.log(endData);
+  // console.log();
+  if(!enemy.data()[0]) {
+    debugger;
+  }
+  var enemyEndX = enemy.data()[0]['x'];
+  var enemyEndY = enemy.data()[0]['y'];
   var enemyStartX = parseFloat(enemy.attr('cx'));
   var enemyStartY = parseFloat(enemy.attr('cy'));
   return function(t) {
@@ -87,6 +110,7 @@ var checkCollisions = function(endData) {
   };
 };
 
+var pursuePlayer = true;
 var inProgress = setInterval(function() {
   for (var i = 0; i < dots.length; i++) {
     dots[i]['x'] = Math.floor(Math.random() * 900);
@@ -97,7 +121,22 @@ var inProgress = setInterval(function() {
     .transition().duration(moveInterval).tween('custom', checkCollisions)
     .attr('cx', function(d) { return d.x; })
     .attr('cy', function(d) { return d.y; });
-    // setScore(false);
+
+  d3.select('.hk').data().x = Math.floor(Math.random() * 900);
+  d3.select('.hk').data().y = Math.floor(Math.random() * 500);
+  if(pursuePlayer || score > 10000) {
+    d3.select('.hk').transition()
+      .duration(moveInterval)
+      //.tween('custom', checkCollisions)
+      .attr('points', hkDraw(parseFloat(player.attr('cx')), parseFloat(player.attr('cy'))));
+    pursuePlayer = false;
+  }else {
+    d3.select('.hk').transition()
+      .duration(moveInterval)
+      //.tween('custom', checkCollisions)
+      .attr('points', hkDraw(Math.floor(Math.random() * 900), Math.floor(Math.random() * 500)));
+    pursuePlayer = true;
+  }
 }, moveInterval);
 
 
